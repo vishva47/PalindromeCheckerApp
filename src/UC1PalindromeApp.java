929d8923ee31bc18ddfdb400b88c7f81494a5780
vishva47 <vishvarethinam123@gmail.com>
2026-03-25 16:31:59 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/UC1PalindromeApp.java
@@ -0,0 +1,8 @@
+public class UC1PalindromeApp {
+    public static void main(String[] args) {
+        System.out.println("====================================");
+        System.out.println("   Palindrome Checker Application");
+        System.out.println("   Version 1.0");
+        System.out.println("====================================");
+    }
+}
